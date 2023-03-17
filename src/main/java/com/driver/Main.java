5d3a03152ded51334c85a34d301dd2f2442bc7f7
jayeshsontakke <jayeshsontakke1998@gmail.com>
2023-03-17 09:49:43 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
-public class Main {
+public  class  Main {
+    public static void main(String []args){
+        Product p=new Product();
+       int ans0= p.product(10,20);
+       int ans1=p.product(10,20,30);
+       double ans2=p.product(10.5,9.7);
+        System.out.println(ans1+" "+ans1+""+ans2);
 
+
+    }
+    public static class Product{
+        public int product(int x, int y) {
+          return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
